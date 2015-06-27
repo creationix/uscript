7f44bab6ea46187707346783722b693ad2e95a2b
Tim Caswell <tim@creationix.com>
2015-06-26 22:42:08 -0500

    Add more tests and opcodes

--- main.c
+++ main.c
@@ -72,12 +72,12 @@ enum opcodes {
   OP_TONE,
 };
 
-size_t eval(uint8_t* pc, int32_t* res) {
-  printf("OP=%02x\n", *pc);
+size_t eval(const uint8_t* pc, int32_t* res) {
 
   // If the high bit is set, it's an opcode index.
   if (*pc & 0x80) {
-    uint8_t* start = pc;
+    const uint8_t* start = pc;
+    printf("OP=%02x\n", *pc);
 
     switch (*pc++) {
 
@@ -86,6 +86,38 @@ size_t eval(uint8_t* pc, int32_t* res) {
       pc += eval(pc, &a);
       pc += eval(pc, &b);
       *res = a + b;
+      return pc - start;
+    }
+
+    case OP_SUB: {
+      int32_t a, b;
+      pc += eval(pc, &a);
+      pc += eval(pc, &b);
+      *res = a - b;
+      return pc - start;
+    }
+
+    case OP_MUL: {
+      int32_t a, b;
+      pc += eval(pc, &a);
+      pc += eval(pc, &b);
+      *res = a * b;
+      return pc - start;
+    }
+
+    case OP_DIV: {
+      int32_t a, b;
+      pc += eval(pc, &a);
+      pc += eval(pc, &b);
+      *res = a / b;
+      return pc - start;
+    }
+
+    case OP_MOD: {
+      int32_t a, b;
+      pc += eval(pc, &a);
+      pc += eval(pc, &b);
+      *res = a % b;
       return pc - start;
     }
 
@@ -105,19 +137,61 @@ size_t eval(uint8_t* pc, int32_t* res) {
     if (!(*pc++ & 0x80)) return 3;
     *res |= (*pc & 0x7f) << 20;
     if (!(*pc++ & 0x80)) return 4;
-    *res |= (*pc & 0x7f) << 27;
+    *res |= (*pc & 0x1f) << 27;
     return 5;
   }
 }
 
-int main() {
-  // add 1 2
-  uint8_t program[] = {
-    OP_ADD, 1, 2
-  };
+static inline void test(const char* description, const uint8_t* program) {
   int32_t out;
   eval(program, &out);
+  printf("%s = %d\n", description, out);
+}
 
-  printf("1 + 2 = %d\n", out);
+// Prints the bytes needed to encode a number.
+// Useful for creating dummy programs.
+void dump(uint32_t i) {
+  if (i < 0x40) {
+    printf("%d\n", i & 0x3f);
+    return;
+  }
+  printf("%d ", (i & 0x3f) | 0x40);
+  i >>= 6;
+  while (i) {
+    if (i < 0x80) {
+      printf("%d\n", i & 0x7f);
+      return;
+    }
+    printf("%d ", (i & 0x7f) | 0x80);
+    i >>= 7;
+  }
+}
+int main() {
+  test("1 + (2 * 3)", (uint8_t[])
+    { OP_ADD, 1, OP_MUL, 2, 3 });
+  test("(1 + 2) * 3", (uint8_t[])
+    { OP_MUL, OP_ADD, 1, 2, 3 });
+  test("10 / 3", (uint8_t[])
+    { OP_DIV, 10, 3 });
+  test("10 % 3", (uint8_t[])
+    { OP_MOD, 10, 3 });
+  test("10", (uint8_t[])
+    { 10 });
+  test("100", (uint8_t[])
+    { 100, 1 });
+  test("1000", (uint8_t[])
+    { 104, 15 });
+  test("10000", (uint8_t[])
+    { 80, 156, 1 });
+  test("100000", (uint8_t[])
+    { 96, 154, 12 });
+  test("1000000", (uint8_t[])
+    { 64, 137, 122 });
+  test("10000000", (uint8_t[])
+    { 64, 218, 196, 9 });
+  test("100000000", (uint8_t[])
+    { 64, 132, 175, 95 });
+  test("1000000000", (uint8_t[])
+    { 64, 168, 214, 185, 7 });
   return 0;
 }
