3af23d9cb71243ff6661a7f81423301bddebf08d
Tim Caswell <tim@creationix.com>
2015-06-28 17:36:18 -0500

    Add unix repl

---
+++ unix-main.c
@@ -0,0 +1,37 @@
+#include "uscript.c"
+#include <stdio.h>
+
+#define KNRM  "\x1B[0m"
+#define KRED  "\x1B[1;31m"
+#define KGRN  "\x1B[1;32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[1;34m"
+#define KMAG  "\x1B[1;35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
+
+
+int main() {
+  uint8_t* line;
+  size_t size;
+  while (1) {
+    printf(KNRM "> " KGRN);
+    if (getline((char**)&line, &size, stdin) < 0) return 0;
+    printf(KNRM);
+    int len = compile(line);
+    if ((int) len < 0) {
+      int offset = 1 - (int)len;
+      while (offset--) printf(" ");
+      printf(KRED "^\n");
+      printf("Unexpected input\n" KNRM);
+      continue;
+    }
+    while (len > 0) {
+      int32_t result;
+      uint8_t* end = eval(line, &result);
+      len -= end - line;
+      line = end;
+      printf("%s%d%s\n", KBLU, result, KNRM);
+    }
+  }
+}
