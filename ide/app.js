4022b80c577680aefb5c7179b6640cb0741403cf
Tim Caswell <tim@creationix.com>
2015-07-09 20:17:27 -0500

    Add more samples

--- ide/app.js
+++ ide/app.js
@@ -5,6 +5,8 @@ var docs = {
   dw: ["Digital Write", "Set an output pin to 0-Off or 1-On"],
   dr: ["Digital Read", "Read the current value of a pin as 0-Off, 1-On"],
   ar: ["Analog Read", "Read the current value of a pin as analog."],
+  aw: ["Analog Write", "Analog write to a pin.  Really it's PWM write."],
+  run: ["Run", "Run a subroutine and return back to here"],
   not: ["Not", "Invert value.  0 -> 1 and non-zero to zero"],
   delay: ["Delay", "Delay for a number of milliseconds"],
   add: ["+", "Add two values", 1],
@@ -21,96 +23,6 @@ var docs = {
   abs: ["|", "Find the absolute value of a value", 2],
 }
 
-// DEF a DO 3 PM 4 1 PM a0 0 WHILE 1 DO 2 DW 4 NOT DR 4 DELAY AR a0
-var def = ["def", 0,
-  ["do", 3,
-    ["pm", 4, 1],
-    ["pm", 17, 0],
-    ["while", 1,
-      ["do", 2,
-        ["dw", 4,
-          ["not",
-            ["dr", 4]
-          ]
-        ],
-        ["delay",
-          ["ar", 17]
-        ]
-      ]
-    ]
-  ]
-];
-
-// DEF 17 DO 7
-//   SET 2 SUB 512 ABS SUB GET 11 512
-//   SET 23 DIV MUL SUB 512 ABS SUB MOD GET 7 1024 512 GET 2 512
-//   IF LT GET 7 512 DO 3
-//     SET 17 GET 2
-//     SET 6 GET 23
-//     SET 1 0
-//   ELIF LT GET 7 1024 DO 3
-//     SET 17 GET 23
-//     SET 6 GET 2
-//     SET 1 0
-//   ELIF LT GET 7 1536 DO 3
-//     SET 17 0
-//     SET 6 GET 2
-//     SET 1 GET 23
-//   ELIF LT GET 7 2048 DO 3
-//     SET 17 0
-//     SET 6 GET 23
-//     SET 1 GET 2
-//   ELIF LT GET 7 2560 DO 3
-//     SET 17 GET 23
-//     SET 6 0
-//     SET 1 GET 2
-//   ELSE DO 3
-//     SET 17 GET 2
-//     SET 6 0
-//     SET 1 GET 23
-//   SET 12 DIV SUB GET 11 GET 2 2
-//   SET 17 ADD GET 17 GET 12
-//   SET 6 ADD GET 6 GET 12
-//   SET 1 ADD GET 1 GET 12
-
-var def2 = ["def", 17, ["do", 7,
-  ["set", 2, ["sub", 512, ["abs", ["sub", ["get", 11], 512]]]],
-  ["set", 23, ["div", ["mul", ["sub", 512, ["abs", ["sub", ["mod", ["get", 7], 1024], 512]]], ["get", 2], ],512]],
-  ["if", ["lt", ["get", 7], 512], ["do", 3,
-    ["set", 17, ["get", 2]],
-    ["set", 6, ["get", 23]],
-    ["set", 1, 0],
-  ]],
-  ["elif", ["lt", ["get", 7], 1024], ["do", 3,
-    ["set", 17, ["get", 23]],
-    ["set", 6, ["get", 2]],
-    ["set", 1, 0],
-  ]],
-  ["elif", ["lt", ["get", 7], 1536], ["do", 3,
-    ["set", 17, 0],
-    ["set", 6, ["get", 2]],
-    ["set", 1, ["get", 23]],
-  ]],
-  ["elif", ["lt", ["get", 7], 2048], ["do", 3,
-    ["set", 17, 0],
-    ["set", 6, ["get", 23]],
-    ["set", 1, ["get", 2]],
-  ]],
-  ["elif", ["lt", ["get", 7], 2560], ["do", 3,
-    ["set", 17, ["get", 23]],
-    ["set", 6, 0],
-    ["set", 1, ["get", 2]],
-  ]],
-  ["else", ["do", 3,
-    ["set", 17, ["get", 2]],
-    ["set", 6, 0],
-    ["set", 1, ["get", 23]],
-  ]],
-  ["set", 12, ["div", ["sub", ["get", 11], ["get", 2]], 2]],
-  ["set", 17, ["add", ["get", 17], ["get", 12]]],
-  ["set", 6, ["add", ["get", 6], ["get", 12]]],
-  ["set", 1, ["add", ["get", 1], ["get", 12]]]
-]];
 function render(ast, depth) {
   if (typeof ast === "number") {
     var node = document.createElement("div");
@@ -137,7 +49,7 @@ function render(ast, depth) {
     }
     node.appendChild(label);
     i = 1;
-    if (type === "get" || type === "set" || type == "def") {
+    if (type === "get" || type === "set" || type == "def" || type == "run") {
       i = 2;
       var child = document.createElement("div");
       child.setAttribute("class", "node variable d" + ((depth + 2)%16));
@@ -158,6 +70,151 @@ function render(ast, depth) {
   return node;
 }
 
+var color = Math.floor(Math.random() * 16);
+function add(def) {
+  color = (color + 3) % 16;
+  document.body.appendChild(render(def, color));
+}
+
 window.onload = function () {
-  document.body.appendChild(render(def2, Math.floor(Math.random() * 16)));
+
+  // DEF 0 DO 8
+  //   PM 12 1
+  //   PM 13 1
+  //   PM 14 1
+  //   PM 5 0
+  //   PM 4 0
+  //   SET 11 512
+  //   RUN 15
+  //   WHILE 1 RUN 11
+  add(["def", 0, ["do", 8,
+    ["pm", 12, 1],
+    ["pm", 13, 1],
+    ["pm", 14, 1],
+    ["pm", 5, 0],
+    ["pm", 4, 0],
+    ["set", 11, 512],
+    ["run", 15],
+    ["while", 1, ["run", 11]]
+  ]]);
+
+  // DEF 11 DO 4
+  //   IF DR 5
+  //     SET 7 MOD ADD GET 7 1 3072
+  //   IF DR 4
+  //     SET 11 MOD ADD GET 11 1 1024
+  //   RUN 15
+  //   DELAY 3
+  add(["def", 11, ["do", 4,
+    ["if", ["dr", 5],
+      ["set", 7, ["mod", ["add", ["get", 7], 1]], 3072]],
+    ["if", ["dr", 4],
+      ["set", 11, ["mod", ["add", ["get", 11], 1]], 1024]],
+    ["run", 15],
+    ["delay", 3]
+  ]]);
+
+  // DEF 15 DO 4
+  //   RUN 17
+  //   AW 12 SUB 1023 GET 17
+  //   AW 13 SUB 1023 GET 6
+  //   AW 14 SUB 1023 GET 1
+
+  add(["def", 15, ["do", 4,
+    ["run", 17],
+    ["aw", 12, ["sub", 1023, ["get", 17]]],
+    ["aw", 13, ["sub", 1023, ["get", 6]]],
+    ["aw", 14, ["sub", 1023, ["get", 1]]],
+  ]]);
+
+  // DEF 17 DO 7
+  //   SET 2 SUB 512 ABS SUB GET 11 512
+  //   SET 23 DIV MUL SUB 512 ABS SUB MOD GET 7 1024 512 GET 2 512
+  //   IF LT GET 7 512 DO 3
+  //     SET 17 GET 2
+  //     SET 6 GET 23
+  //     SET 1 0
+  //   ELIF LT GET 7 1024 DO 3
+  //     SET 17 GET 23
+  //     SET 6 GET 2
+  //     SET 1 0
+  //   ELIF LT GET 7 1536 DO 3
+  //     SET 17 0
+  //     SET 6 GET 2
+  //     SET 1 GET 23
+  //   ELIF LT GET 7 2048 DO 3
+  //     SET 17 0
+  //     SET 6 GET 23
+  //     SET 1 GET 2
+  //   ELIF LT GET 7 2560 DO 3
+  //     SET 17 GET 23
+  //     SET 6 0
+  //     SET 1 GET 2
+  //   ELSE DO 3
+  //     SET 17 GET 2
+  //     SET 6 0
+  //     SET 1 GET 23
+  //   SET 12 DIV SUB GET 11 GET 2 2
+  //   SET 17 ADD GET 17 GET 12
+  //   SET 6 ADD GET 6 GET 12
+  //   SET 1 ADD GET 1 GET 12
+  add(["def", 17, ["do", 7,
+    ["set", 2, ["sub", 512, ["abs", ["sub", ["get", 11], 512]]]],
+    ["set", 23, ["div", ["mul", ["sub", 512, ["abs", ["sub", ["mod", ["get", 7], 1024], 512]]], ["get", 2], ],512]],
+    ["if", ["lt", ["get", 7], 512], ["do", 3,
+      ["set", 17, ["get", 2]],
+      ["set", 6, ["get", 23]],
+      ["set", 1, 0],
+    ]],
+    ["elif", ["lt", ["get", 7], 1024], ["do", 3,
+      ["set", 17, ["get", 23]],
+      ["set", 6, ["get", 2]],
+      ["set", 1, 0],
+    ]],
+    ["elif", ["lt", ["get", 7], 1536], ["do", 3,
+      ["set", 17, 0],
+      ["set", 6, ["get", 2]],
+      ["set", 1, ["get", 23]],
+    ]],
+    ["elif", ["lt", ["get", 7], 2048], ["do", 3,
+      ["set", 17, 0],
+      ["set", 6, ["get", 23]],
+      ["set", 1, ["get", 2]],
+    ]],
+    ["elif", ["lt", ["get", 7], 2560], ["do", 3,
+      ["set", 17, ["get", 23]],
+      ["set", 6, 0],
+      ["set", 1, ["get", 2]],
+    ]],
+    ["else", ["do", 3,
+      ["set", 17, ["get", 2]],
+      ["set", 6, 0],
+      ["set", 1, ["get", 23]],
+    ]],
+    ["set", 12, ["div", ["sub", ["get", 11], ["get", 2]], 2]],
+    ["set", 17, ["add", ["get", 17], ["get", 12]]],
+    ["set", 6, ["add", ["get", 6], ["get", 12]]],
+    ["set", 1, ["add", ["get", 1], ["get", 12]]]
+  ]]);
+
+  // DEF a DO 3 PM 4 1 PM a0 0 WHILE 1 DO 2 DW 4 NOT DR 4 DELAY AR a0
+  add(["def", 25,
+    ["do", 3,
+      ["pm", 4, 1],
+      ["pm", 17, 0],
+      ["while", 1,
+        ["do", 2,
+          ["dw", 4,
+            ["not",
+              ["dr", 4]
+            ]
+          ],
+          ["delay",
+            ["ar", 17]
+          ]
+        ]
+      ]
+    ]
+  ]);
+
 };
