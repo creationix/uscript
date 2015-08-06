51e9925570465ca25cae6f4823426b78788027f1
Tim Caswell <tim@creationix.com>
2015-08-05 21:10:55 -0500

    Flesh out example

--- examples/simple/simple.ino
+++ examples/simple/simple.ino
@@ -1,15 +1,26 @@
 #include "uscript.h"
 
+#define LED_PIN1 2
+#define LED_PIN2 6
+#define LED_PIN3 A1
+#define INPUT_PIN A0
+
 unsigned char code1[] = {
-  PM, 13, 1,         // pinMode(13, OUTPUT)
+  PM, LED_PIN1, OUTPUT,  // pinMode(LED_PIN1, OUTPUT)
+  PM, LED_PIN2, OUTPUT,  // pinMode(LED_PIN2, OUTPUT)
+  PM, LED_PIN3, OUTPUT,  // pinMode(LED_PIN3, OUTPUT)
+  PM, INPUT_PIN, INPUT,  // pinMode(INPUT_PIN, INPUT)
   END
 };
 
 unsigned char code2[] = {
-  W0, DR, 13,        // a = digitalRead(13)
-  PRINT, R0,         // Serial.println(a)
-  DW, 13, NOT, R0,   // digitalWrite(14, !a)
-  DELAY, 0x43, 0x74, // delay(500)
+  W1, AR, INPUT_PIN,
+  W0, DR, LED_PIN1,      // a = digitalRead(LED_PIN1)
+  PRINT, R0,             // Serial.println(a)
+  DW, LED_PIN1, NOT, R0, // digitalWrite(LED_PIN1, !a)
+  DW, LED_PIN2, R0,      // digitalWrite(LED_PIN1, a)
+  DW, LED_PIN3, NOT, DR, LED_PIN3,
+  DELAY, R1,     // delay(b)
   END
 };
 
